458e53dfed58a69a7a2de947f64fe78db6104361
stone9693 <fengxi9693@163.com>
2020-07-29 17:39:15 +0800

    Update 200729.py

--- tricks/200729.py
+++ tricks/200729.py
@@ -4,3 +4,10 @@ dict1={'a': 24, 'g': 52, 'i': 12, 'k': 33}
 sorted(dict1.items(),key=lambda x:x[1])
 # 字典生成式
 dict2={key:value for key,value in sorted(dict1.items(),key=lambda x:x[1])}
+# 倒叙排列string
+str1='abcde'
+str2=str1[::-1]
+# map
+list1=[1, 2, 3]
+list2=[4, 5, 6]
+iter=map(lambda x1,x2:x1+x2,list1,list2)
